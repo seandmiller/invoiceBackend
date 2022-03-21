4fefde18878789597f63e1f26f3c848cc4414fcb
aseani miller <milleraseani@gmail.com>
2022-03-21 12:28:08 -0700

    heroku push

--- index.js
+++ index.js
@@ -55,14 +55,14 @@ app.post('/send_mail', async (req,res) => {
 
 
 
-process.once('SIGUSR2', function () {
-    process.kill(process.pid, 'SIGUSR2');
-  });
+// process.once('SIGUSR2', function () {
+//     process.kill(process.pid, 'SIGUSR2');
+//   });
   
-  process.on('SIGINT', function () {
-    // this is only called on ctrl+c, not restart
-    process.kill(process.pid, 'SIGINT');
-  });
+//   process.on('SIGINT', function () {
+//     // this is only called on ctrl+c, not restart
+//     process.kill(process.pid, 'SIGINT');
+//   });
 
 
 app.listen(8282, () => console.log('listening'))
