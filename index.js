32f3d812295a3290ade5b4aa76d96b8ce4b4d6ab
aseani miller <milleraseani@gmail.com>
2022-04-15 00:24:03 -0700

    fetching

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const cors = require('cors');
+const cors = require('cors');
 require('dotenv').config()
 const express = require('express');
 
@@ -10,7 +10,7 @@ const app = express();
 
 // app.use(express.json());
 app.use(cors({
-    origin:'https://www.omillerwieldinginvoice.com',
+    origin:'*'
 
 }))
 
