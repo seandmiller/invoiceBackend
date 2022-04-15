d59611be403249e32be8cb85eca09e6fe4baec58
aseani miller <milleraseani@gmail.com>
2022-04-15 10:56:14 -0700

    pe

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    allowedHeaders:["authorization", 'Content-Type'],
-    exposedHeaders: ['authorization'],
+    allowedHeaders:["*"],
+    exposedHeaders: ['*'],
     origin:'https://www.omillerwieldinginvoice.com/',
     headers:'*',
     preflightContinue:false,
