aff176a9bacccadfdadf78d445576f80752df362
aseani miller <milleraseani@gmail.com>
2022-04-14 22:29:12 -0700

    fetching

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin:'https://www.omillerwieldinginvoice.com/',
+    origins:'https://www.omillerwieldinginvoice.com/',
 }))
 
 
