092f08c68f4a0e9c03ce8e51d1bd0301136d2e5e
aseani miller <milleraseani@gmail.com>
2022-04-19 12:28:41 -0700

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({origin:"https://invoiceappnodejs.herokuapp.com/send_mail"}));
+app.use(cors({origin:"*"}));
 
 app.use(express.json());
 app.options('/send_mail', cors());
